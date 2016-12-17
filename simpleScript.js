c4cd8d71b7e79c53571de5bd97f5a5bd7ab8eaa8
kalojandg <kalojandg@gmail.com>
2016-12-17 11:22:06 +0200

    Commit from sourcetree

---
+++ simpleScript.js
@@ -0,0 +1,102 @@
+﻿function submitClicked() {
+    window.open("intro2.html");
+}
+function onNameEnter() {
+    //alert("You chose to enter first name!");
+    window.resizeBy(0.5);
+}
+function onLastNameEnter() {
+    //alert("You chose to enter last name!");
+}
+var invis = document.getElementById("invisibleDiv");
+invis.style.visibility = 'hidden';
+function onPictureclick() {
+    //alert("Picture clicked");
+    //Watch-out-MEME-500x375
+    var domAlone = document.getElementById("imgForeverAlone");
+    domAlone.src = "Watch-out-MEME-500x375.jpg";
+    domAlone.style.borderStyle = 'solid';
+    changeHandler();
+}
+function onOver() {
+    document.body.appendChild(document.createElement("div", "artificiallyCreatedDiv"));
+    var domAlone = document.getElementById("imgForeverAlone");
+    domAlone.title = "This is added tooltip";
+}
+function changeHandler() {
+    var domAlone = document.getElementById("imgForeverAlone");
+    domAlone.setAttribute("onclick", "changedHandler()");
+}
+function changedHandler() {
+    var domAlone = document.getElementById("imgForeverAlone");
+    if (domAlone.src == "file:///C:/Users/kaloyan.georgiev/Desktop/intro/happy_forever_alone_by_ninnymuffin-d55o62c.png")
+        domAlone.setAttribute("src", "Watch-out-MEME-500x375.jpg")
+    else
+        domAlone.setAttribute("src", "happy_forever_alone_by_ninnymuffin-d55o62c.png");
+}
+function createShtuff() {
+    var newDiv = document.createElement("div", "artificiallyAddedDiv");
+    newDiv.style.width = 1000;
+    newDiv.style.height = 700;
+    var domAlone = document.getElementById("imgForeverAlone");
+    newDiv.appendChild(domAlone);
+}
+var timer;
+function enableTimer() {
+    timer = setInterval(moveAround, 400);
+}
+function disableTimer() {
+    clearInterval(timer);
+}
+function moveAround() {
+    var formDiv = document.getElementById("formDiv");
+    //alert(formDiv.style.bottom);
+    //formDiv.style.bottom = (formDiv.style.bottom + Math.random()) ;\
+    for (var i = 0; i < 100; i++) { 
+    var number = parseInt(Math.random() * (window.innerHeight - formDiv.clientHeight) )-5;
+        formDiv.style.left = number + "px";
+        formDiv.style.top = number + "px";
+    //location.reload();
+        //console.log(number);
+        //console.log(formDiv.style.top + formDiv.style.left);
+        
+    }
+    
+}
+var formDiv = document.getElementById("formDiv");
+formDiv.onmouseleave = logOut;
+function logOut() {
+    //console.log("mouse exited");
+}
+window.addEventListener("click", onDocumentclick);
+function onDocumentclick(event) {
+    var x = event.clientX;
+    var y = event.clientY;
+    //alert(document.location.protocol);
+    //console.log(x, y);
+    var invis = document.getElementById("invisibleDiv");
+    //console.log(invis.style.top);
+    invis.style.visibility = 'visible';
+    invis.style.marginLeft = x + "px";
+    invis.style.marginTop = y + "px";
+    invis.style.backgroundColor = "red";
+}
+//brace yourself
+var requests = new XMLHttpRequest();
+if (!requests) alert("mazalo");
+
+
+requests.open("GET", "respond.txt", true);
+requests.send(null);
+
+//requests.onload = function () {
+//    divInDiv.innerHTML = requests.responseText;
+//}
+//alert(requests.responseText);
+//alert(divInDiv);
+var divInDiv = document.getElementById("formDiv");
+
+requests.onreadystatechange = function () {
+    if(this.status= 200)
+        divInDiv.innerHTML = requests.responseText;
+}
---
+++ simpleScriptBase.js
@@ -0,0 +1,116 @@
+﻿var a = 3923;
+var b = "sd";
+var c = a + b;
+//alert("script enabled");
+
+function construct(c) {
+    this.val = c;
+    this.prop = null;
+}
+
+var testValue = new Object("new value");
+var newByFunc = new construct(15);
+var jsonLikeObject = {
+    prop1: "valueofProp1",
+    prop2: "valueOfProp2"
+
+}
+var bools = Boolean(-2);
+bools = !!0;
+
+var funcDifferences = new function () { };
+//alert(funcDifferences);//object
+document.write("string");
+document.write("<div><b> Div inserted </b></div>")
+var squareObj = { "Uppercorner": {x : 10, y : 30},
+                  "lowercorner": {x : 40, y :50}
+};
+document.writeln(squareObj.Uppercorner.x + "<br>");
+
+var firstArr = new Array();
+var almostFixedSize = new Array(10);
+firstArr[0] = 0;
+firstArr[1] = 1;
+firstArr[3] = 3;
+almostFixedSize[11] = 15;
+document.write("empty element is " + firstArr[2] + "<br>");
+document.write("12th element is " + almostFixedSize[11] + "<br>");
+
+if (undefined == null )
+    document.write("NULL===false" + "<br>");
+
+var stringTypes = "this is string, which is type: ";
+var stringTypeTwo = new String("this is the scond type, which is: ");
+document.write(stringTypes + typeof (stringTypes) + "<br>");
+document.write(stringTypeTwo + typeof (stringTypeTwo) + "<br>");
+
+//document.write("transition to object result: " + typeof (Object(stringTypeTwo)) + "<br>");
+//document.write(" transition to string result: " + typeof (stringTypes.valueOf()) + "<br>")
+//var arr = new Array();
+//document.write(arr.toString + " " +typeof (arr) + "<br>")
+//var arrTwo = [];
+//document.write (arrTwo.toString + " " + typeof (arrTwo) + "<br>")
+//var num = new Number(10);
+//function passNumberByRef(num) {
+//    num.valueOf = 15;
+//}
+//passNumberByRef(num);
+//document.write("the new value of num is: " + num.valueOf + "<br>");
+
+//outer: for (var i = 0; i < 100; i++) {
+//    for (var j = 200; j < 250; j++){
+//        document.write("i: " + i + " " + "j: " + j + "<br>");
+//        if (j == 230)
+//            break outer;
+//    }
+//}
+function returningUndefined() {
+    //alert("functionBody");
+    
+}
+var res = returningUndefined();
+//alert(res);
+document.write(typeof res);
+var p = 10;
+if (function () {
+    return p == 10;
+    })
+    {
+    //function f (p){
+    //    alert(p);
+    //}
+    //alert(p);
+}
+function caller(x) {
+    //alert(y);
+    //alert(function () {
+    //    var y = 19;
+    //   return x;
+    //}());
+}
+caller(17);
+var ourDiv = document.getElementById("di");
+ourDiv.innerText = "I've got the power";
+ourDiv.style.backgroundColor = "red";
+
+function ClassConstruct() {
+
+    this.prop = "prop value";
+}
+ClassConstruct.prototype.staticFunction =new function () {
+    prompt("this is the static function");
+}
+var val = new ClassConstruct();
+val.prop = 15;
+//document.write(val.staticFunction);
+var secondObject = new ClassConstruct();
+//document.write(secondObject.staticFunction());
+//document.write("<br>" + secondObject.prop + " " + secondObject.staticFunction + "<br>");
+val.staticFunction= function () {
+    alert(" function changed through first object");
+}
+//document.write("<br>" + secondObject.prop + " " + secondObject.staticFunction + "<br>");
+//for (property in val)
+//    document.write("<br>" + property);
+
+//document.write(ClassConstruct.staticFunction());
